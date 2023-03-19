9e061644a0edfde04180838074d649dc1323b5ce
tkellys <thamireskellysantos@gmail.com>
2023-03-19 19:34:29 -0300

    tarefa resolvida

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,12 +1,44 @@
 
-let listaAfazeres = document.getElementById("itemDigitado");
+// Com forEch ??
+
+
+
+// Com metodo appendChild - funciona mas perde a config
 
  function pushItem(){
-    // SE O VALOR FOR DIFERENTE DE VAZIO CRIAR UM ARRAY COM LISTAS, pode adicionar ocm push
-    // laço com for each co variavel q vai giuardar. pesquisar metodo append shold
-   let tasks = (`<li class="list-group-item">${listaAfazeres.value}</li>`);
-   let ul = document.getElementById("lista").innerHTML = tasks;
-    
+  let listaAfazeres = document.getElementById("itemDigitado");
+  let ulLista = document.getElementById('lista');
+  let novoLi = document.createElement('li');
+  let checkbox = document.createElement('input');
+  let cxTexto = document.createElement('label');
+
+  cxTexto.innerText = listaAfazeres.value;
+  checkbox.type = "checkbox";
+  checkbox.className = "form-check-input me-1";
+  cxTexto.className = "form-check-label";
+  novoLi.className = "list-group-item";
+
+  novoLi.appendChild(checkbox);
+  novoLi.appendChild(cxTexto);
+  
+  ulLista.appendChild(novoLi);
+  
+  listaAfazeres.value = "";
  }
 
- // DEVER DE CASA ENVIAR ELEMENTOS DE LISTAS PARA  
+
+
+
+
+
+
+
+
+//  // DEVER DE CASA ENVIAR ELEMENTOS DE LISTAS PARA  
+
+//      // SE O VALOR FOR DIFERENTE DE VAZIO CRIAR UM ARRAY COM LISTAS, pode adicionar ocm push
+//     // laço com for each co variavel q vai giuardar. pesquisar metodo append shold
+
+
+//    //let tasks = (`<li class="list-group-item">${listaAfazeres.value}</li>`);
+//    //let ul = document.getElementById("lista").innerHTML = tasks;
